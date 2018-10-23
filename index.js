57680c0467d5b2c0dc6f4114bfe58a203dab3c7e
William Njie <wsnjie@gmail.com>
2018-10-23 01:57:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ return `Happy holidays, ${name}!`
 function happyHolidayTo (holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
+function holidayCountdown (holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
