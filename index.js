28988fcb39a34e812ecf7423204e29ad48c15e17
William Njie <wsnjie@gmail.com>
2018-10-23 01:56:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
-function happyHolidaysTo (name = "you"){
-  return "Happy holidays, ${name}!"
+function happyHolidaysTo (name){
+return `Happy holidays, ${name}!`
 }
 function happyHolidayTo (holiday, name){
   return `Happy ${holiday}, ${name}!`
